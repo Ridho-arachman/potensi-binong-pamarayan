efddac3bc048681b35d0c6319e2b2587af761e97
Binongpamaryan <pamarayandesabinong@gmail.com>
2025-07-23 19:34:08 +0700

    fix: memperbaiki halaman [id] potensi 6

--- src/app/(user)/potensi/[id]/page.tsx
+++ src/app/(user)/potensi/[id]/page.tsx
@@ -1,11 +1,14 @@
 import PotensiDetailClient from "@/components/PotensiDetailClient";
 
-export default async function PotensiDetailPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function PotensiDetailPage({ params }: Props) {
   const { id } = params;
+
   return (
     <section className="container py-8 sm:py-12 md:py-14 px-4 sm:px-6">
       <PotensiDetailClient id={id} />
